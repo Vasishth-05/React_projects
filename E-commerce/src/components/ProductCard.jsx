603ae5549134a3f924d8fa5ddd1f9fea10af07e0
Vasishth <learnvasishth@gmail.com>
2026-05-12 16:47:18 +0530

    Add Tailwind for  styling and structring

--- E-commerce/src/components/ProductCard.jsx
+++ E-commerce/src/components/ProductCard.jsx
@@ -1,11 +1,12 @@
 function ProductCard({product}){
     return <div>
-        <div>{`
+        <div class='' >{`
             ID:${product.id},
             Product_title:${product.title},
             Product_price:${product.price},
             Product_category:${product.category}`
         }</div>
+        <button>Add to cart</button>
     </div>
 }
 
