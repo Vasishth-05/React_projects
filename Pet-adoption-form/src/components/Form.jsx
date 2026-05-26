340da504e9d93dfc440dc8ae65f484b128456327
Vasishth <learnvasishth@gmail.com>
2026-05-26 16:59:59 +0530

    form made

---
+++ Pet-adoption-form/src/components/Form.jsx
@@ -0,0 +1,18 @@
+function Form () {
+    return (
+        <form>
+            <div>Pet Name</div>
+            <input type="text" placeholder="pet name" />
+            <div>Pet Type</div>
+            <input type="text" placeholder="Pet Type" />
+            <div>Pet Breed</div>
+            <input type="text" placeholder="pet Breed" />
+            <div>Your Name</div>
+            <input type="text" placeholder="Your Name" />
+            <div>Phone Number</div>
+            <input type="text" placeholder="Phone Number" />
+        </form>
+    )
+}
+
+export default Form;
