6df9e6bf318e09257162982c72c7aca608bf342a
Vasishth <learnvasishth@gmail.com>
2026-05-26 19:42:11 +0530

    add Table and form created

--- Pet-adoption-form/src/App.jsx
+++ Pet-adoption-form/src/App.jsx
@@ -1,12 +1,13 @@
-
 import './App.css'
 import Form from './components/Form'
+import BasicTable from './components/Table'
 
 function App() {
 
   return (
     <>
       <Form/>
+      <BasicTable/>
     </>
   )
 }
--- Pet-adoption-form/src/components/Form.jsx
+++ Pet-adoption-form/src/components/Form.jsx
@@ -1,17 +1,35 @@
 function Form () {
     return (
-        <form>
-            <div>Pet Name</div>
-            <input type="text" placeholder="pet name" />
-            <div>Pet Type</div>
-            <input type="text" placeholder="Pet Type" />
-            <div>Pet Breed</div>
-            <input type="text" placeholder="pet Breed" />
-            <div>Your Name</div>
-            <input type="text" placeholder="Your Name" />
-            <div>Phone Number</div>
-            <input type="text" placeholder="Phone Number" />
-        </form>
+        <div>
+            <form className="flex-col justify-end items-center">
+                <div>Pet Name</div>
+                <input 
+                    type="text" 
+                    placeholder="pet name" 
+                    className="border-2 border-black-600 mb-4 " />
+                <div>Pet Type</div>
+                <input 
+                    type="text" 
+                    placeholder="Pet Type"
+                     className="border-2 border-black-600 mb-4" />
+                <div>Pet Breed</div>
+                <input 
+                    type="text" 
+                    placeholder="pet Breed"
+                     className="border-2 border-black-600 mb-4" />
+                <div>Your Name</div>
+                <input 
+                    type="text" 
+                    placeholder="Your Name"
+                    className="border-2 border-black-600 mb-4" />
+                <div>Phone Number</div>
+                <input 
+                    type="text" 
+                    placeholder="Phone Number"
+                    className="border-2 border-black-600 mb-4" />
+            </form>
+            <button className="border-2 border-black-600 mb-4 w-32 bg-green-500 hover:bg-green-400">Submit</button>
+        </div>
     )
 }
 
